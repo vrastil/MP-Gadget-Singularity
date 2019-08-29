0035541e333d753ee411f7ce97f8d83fb6f20201
vrastil <michalvrastil@seznam.cz>
2019-08-29 16:09:06 +0200

    add receipt for image with python packages for analyses

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# load singularity if not already
 module load singularity
+
+# build MP-Gadget
 sudo $(which singularity) build -F MP-Gadget.sif mp-gadget.def
 singularity sign MP-Gadget.sif
 singularity push MP-Gadget.sif library://vrastil/default/fast_mp_gadget
+
+
+# build PY-Gadget
+sudo $(which singularity) build -F PY-Gadget.sif py-gadget.def
+singularity sign PY-Gadget.sif
+singularity push MP-Gadget.sif library://vrastil/default/py_gadget
