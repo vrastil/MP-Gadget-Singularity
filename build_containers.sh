9e9ffde20709aa2a22f955c75d9239782b01ae38
vrastil <vrastil@fzu.cz>
2019-09-04 17:17:55 +0200

    add build script, rename

--- build_containers.sh
+++ build_containers.sh
@@ -3,13 +3,14 @@
 # load singularity if not already
 module load singularity
 
-# build MP-Gadget
+# build containers
 sudo $(which singularity) build -F MP-Gadget.sif mp-gadget.def
-singularity sign MP-Gadget.sif
-singularity push MP-Gadget.sif library://vrastil/default/fast_mp_gadget
-
-
-# build PY-Gadget
 sudo $(which singularity) build -F PY-Gadget.sif py-gadget.def
+
+# sign containers
+singularity sign MP-Gadget.sif
 singularity sign PY-Gadget.sif
+
+#push containers
+singularity push MP-Gadget.sif library://vrastil/default/fast_mp_gadget
 singularity push PY-Gadget.sif library://vrastil/default/py_gadget
---
+++ build_mp_gadget.sh
@@ -0,0 +1,4 @@
+# build MP-Gadget from container
+# run this script from the top-level directory (MP-GADGET)
+module load singularity
+singularity exec containers/singularity_v3/MP-Gadget.sif make
